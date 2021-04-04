4fbf269945c36ab28ffc15f984fe0d469ea0153e
Alexander Pinske <alex@pinske.eu>
2021-04-04 13:33:26 +0200

    inital

---
+++ src/main/java/eu/pinske/playground/playground/mail/PlaygroundMailApplication.java
@@ -0,0 +1,13 @@
+package eu.pinske.playground.playground.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundMailApplication.class, args);
+	}
+
+}
